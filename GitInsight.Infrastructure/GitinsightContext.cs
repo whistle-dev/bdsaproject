2bd4cff721f4dcf1c8576768c4f709f1be5c3207
Max <maxt@itu.dk>
2022-11-15 14:25:01 +0100

    Refactor db (again), fix repo primary key and add latest commit sha

--- GitInsight.Infrastructure/GitinsightContext.cs
+++ GitInsight.Infrastructure/GitinsightContext.cs
@@ -12,44 +12,30 @@ namespace GitInsight.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {            
             modelBuilder.Entity<Repo>(r => {
-                r.HasKey(r => r.Hash);
-                r.Property(r => r.Hash).ValueGeneratedNever();
+                r.HasKey(r => r.Path);
+                r.Property(r => r.Path).ValueGeneratedNever();
+                r.Property(r => r.Path).IsRequired();
+                r.HasIndex(r => r.Path).IsUnique();
 
-                r.HasIndex(r => r.Hash).IsUnique();
-
-                r.HasMany(x => x.Commits).WithOne(c => c.Repo).HasForeignKey(c => c.RepoHash);
-
-                r.Property(r => r.Hash)
-                    .IsRequired();
-
-                r.Property(r => r.Name)
-                    .IsRequired();
+                r.HasMany(x => x.Commits).WithOne(c => c.Repo).HasForeignKey(c => c.RepoPath);
             });
 
             modelBuilder.Entity<Commit>(c => {
-                c.HasKey(c => c.Hash);
-                c.Property(c => c.Hash).ValueGeneratedNever();
-                
-                c.HasIndex(c => c.Hash).IsUnique();
+                c.HasKey(c => c.Sha);
+                c.Property(c => c.Sha).ValueGeneratedNever();
+                c.Property(c => c.Sha).IsRequired();
+                c.HasIndex(c => c.Sha).IsUnique();
 
-                c.HasOne(c => c.Repo).WithMany(r => r.Commits).HasForeignKey(c => c.RepoHash);
+                c.HasOne(c => c.Repo).WithMany(r => r.Commits).HasForeignKey(c => c.RepoPath);
 
-                c.HasIndex(c => c.RepoHash);
+                c.HasIndex(c => c.RepoPath);
+                c.Property(c => c.RepoPath).IsRequired();
 
-                c.Property(c => c.Hash)
-                    .IsRequired();
+                c.Property(c => c.Message).IsRequired();
 
-                c.Property(c => c.Message)
-                    .IsRequired();
+                c.Property(c => c.Date).IsRequired();
 
-                c.Property(c => c.Date)
-                    .IsRequired();
-
-                c.Property(c => c.Author)
-                    .IsRequired();
-
-                c.Property(c => c.RepoHash)
-                    .IsRequired();
+                c.Property(c => c.Author).IsRequired();
             });
         }
     }
--- GitInsight.Infrastructure/Repositories/CommitRepository.cs
+++ GitInsight.Infrastructure/Repositories/CommitRepository.cs
@@ -8,9 +8,9 @@ public class CommitRepository : ICommitRepository
 
     public void Create(CommitCreateDTO commit)
     {
-        var isCommitInDb = _context.Commits.Any(c => c.Hash == commit.Hash);
+        var isCommitInDb = _context.Commits.Any(c => c.Sha == commit.Sha);
 
-        var isRepoInDb = _context.Repos.Any(r => r.Hash == commit.RepoHash);
+        var isRepoInDb = _context.Repos.Any(r => r.Path == commit.RepoPath);
 
         if (isCommitInDb)
         {
@@ -22,25 +22,35 @@ public class CommitRepository : ICommitRepository
         }
         else
         {
-            var commitEntity = new Commit(commit.Hash, commit.Message, commit.Date, commit.Author) { RepoHash = commit.RepoHash };
+            var commitEntity = new Commit(commit.Sha, commit.Message, commit.Date, commit.Author, commit.RepoPath);
 
             _context.Commits.Add(commitEntity);
             _context.SaveChanges();
         }
     }
 
-    public CommitDTO? Find(int hash)
+    public CommitDTO? Find(string sha)
     {
         var commit = from c in _context.Commits
-                     where c.Hash == hash
-                     select new CommitDTO(c.Hash, c.Message, c.Date, c.Author, c.RepoHash!);
+                     where c.Sha == sha
+                     select new CommitDTO(c.Sha, c.Message, c.Date, c.Author, c.RepoPath);
 
         return commit.FirstOrDefault();
     }
 
-    public IReadOnlyCollection<CommitDTO> ReadAllInRepo(int repo)
+    public CommitDTO? FindLatestInRepo(string repoPath)
     {
-        var repoExists = _context.Repos.Any(r => r.Hash == repo);
+        var commit = from c in _context.Commits
+                     where c.RepoPath == repoPath
+                     orderby c.Date descending
+                     select new CommitDTO(c.Sha, c.Message, c.Date, c.Author, c.RepoPath);
+
+        return commit.FirstOrDefault();
+    }
+
+    public IReadOnlyCollection<CommitDTO> ReadAllInRepo(string repoPath)
+    {
+        var repoExists = _context.Repos.Any(r => r.Path == repoPath);
 
         if (!repoExists)
         {
@@ -49,8 +59,8 @@ public class CommitRepository : ICommitRepository
         else
         {
             var commits = from c in _context.Commits
-                          where c.RepoHash == repo
-                          select new CommitDTO(c.Hash, c.Message, c.Date, c.Author, c.RepoHash!);
+                          where c.RepoPath == repoPath
+                          select new CommitDTO(c.Sha, c.Message, c.Date, c.Author, c.RepoPath);
 
             return commits.ToList();
         }
