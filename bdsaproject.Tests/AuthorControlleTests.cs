5aa4d929f0a49ab8353afad22721ed14f59f5741
yovitus <vime@itu.dk>
2022-11-15 13:06:12 +0100

    start testing controller

---
+++ bdsaproject.Tests/AuthorControlleTests.cs
@@ -0,0 +1,23 @@
+namespace app.Tests;
+
+public AuthorControllerTests
+{   
+    private readonly IRepository<Commit> _repository;
+    private readonly AuthorController _controller;
+    public void GetAuthorTest()
+    {
+        var _controller = new AuthorController();
+        var _repository = 
+    }
+}
+
+[Fact]
+public void getReturnsOk()
+{
+    // Arrange
+    
+    // Act
+    
+    // Assert with FluentAssertions
+  
+}
